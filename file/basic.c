69593fdea1240972ed51621675377e0cee99695b
Mahedi205339 <mahedi205339@gmail.com>
2024-06-01 22:33:27 +0600

    addd some more fundamentels from file

--- file/basic.c
+++ file/basic.c
@@ -6,16 +6,55 @@ int main()
     FILE *fptr;
 
     // fopen("filename" , mode) --> mode shows the what is going to do with this  file (read or write etc)
-    fptr = fopen("text.txt", "w"); // "r" ---> read
+    fptr = fopen("test.txt", "w"); // "r" ---> read
 
-    if (fptr == NULL)
-    {
-        printf("file doesn't exist \n");
-    }
-    else
-    {
-        fclose(fptr); // file --->> close
-    }
+    // if (fptr == NULL)
+    // {
+    //     printf("file doesn't exist \n");
+    // }
+    // else
+    // {
+    //     fclose(fptr); // file --->> close
+    // }
+
+    // int ch;
+    // fscanf(fptr , "%d" ,&ch);
+    // printf("character = %d \n" , ch);
+
+    // fscanf(fptr , "%d" ,&ch);
+    // printf("character = %d \n" , ch);
+
+    // fscanf(fptr , "%d" ,&ch);
+    // printf("character = %d \n" , ch);
+    // fscanf(fptr , "%d" ,&ch);
+    // printf("character = %d \n" , ch);
+
+    // fscanf(fptr , "%d" ,&ch);
+    // printf("character = %d \n" , ch);
+    // fscanf(fptr , "%d" ,&ch);
+    // printf("character = %d \n" , ch);
+
+    // fprintf(fptr, "%c", 'M');
+    // fprintf(fptr, "%c", 'A');
+    // fprintf(fptr, "%c", 'N');
+    // fprintf(fptr, "%c", 'G');
+    // fprintf(fptr, "%c", 'O');
+
+
+    // printf("%c \n",fgetc(fptr));
+    // printf("%c \n",fgetc(fptr));
+    // printf("%c \n",fgetc(fptr));
+    // printf("%c \n",fgetc(fptr));
+    // printf("%c \n",fgetc(fptr));.
+
+    fputc('A', fptr);
+    fputc('P', fptr);
+    fputc('P', fptr);
+    fputc('L', fptr);
+    fputc('E', fptr);
+
+
+  
 
     return 0;
 }
