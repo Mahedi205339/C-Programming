ca90ef55c4ff2555ba2b6a50a72ff96c0b3ea6da
Mahedi205339 <mahedi205339@gmail.com>
2024-06-01 12:47:11 +0600

    added consonant on the same way

--- string/vowels.c
+++ string/vowels.c
@@ -12,12 +12,17 @@ int main()
 int countVowels(char str[])
 {
     int count = 0;
+    int consonant = 0;
     for (int i = 0; str[i] != '\0'; i++)
     {
         if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
         {
             count++;
         }
+        else{
+            consonant ++;
+        }
     }
-    printf("total vowels : %d", count);
+    printf("total vowels : %d \n", count);
+    printf("total vowels : %d \n", consonant);
 }
